383c89fb800aa056a13e73b6f87070f924fc1dd2
Veronika ArtIvanika <petrovskaya.veronika@gmail.com>
2023-05-07 06:33:34 +0300

    class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="footer">
-      <p class="footer__author">© {new Date().getFullYear()} Mesto Russia</p>
+    <footer className="footer">
+      <p className="footer__author">© {new Date().getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
